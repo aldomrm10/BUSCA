931b97cce17836df72bf2796db7315a9f4614391
arogliero3 <arogliero3@gatech.edu>
2020-02-16 21:49:48 -0500

    Adding changes that include images and navigation for the App

---
+++ BUSCA_Project/BUSCA App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BUSCA App
+//
+//  Created by Aldo Mario Rogliero on 2/5/20.
+//  Copyright © 2020 Georgia Institute of Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
