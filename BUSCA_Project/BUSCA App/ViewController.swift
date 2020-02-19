a724ee6007fd6bec2bd72f25074b60ac99878393
arogliero3 <arogliero3@gatech.edu>
2020-02-19 14:35:39 -0500

    Added code for updating labels in a scalable way

--- BUSCA_Project/BUSCA App/ViewController.swift
+++ BUSCA_Project/BUSCA App/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isAvailable = true
     override func viewDidLoad() {
+        updateLabels()
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBOutlet var culc3: [UILabel]!
     
+    func getStatus(id: String?) -> Bool {
+        if (id == nil) {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    
+    func updateLabels() {
+        for table in culc3 {
+            if (getStatus(id: table.restorationIdentifier)) {
+                table.text="🟩"
+            } else {
+                table.text="🟥"
+            }
+        }
+    
+    }
     
 
 }
