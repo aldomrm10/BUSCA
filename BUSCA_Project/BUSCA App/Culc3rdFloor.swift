c23f2894f5d000a702eb8389eac16bb7d78d24d1
arogliero3 <arogliero3@gatech.edu>
2020-02-25 01:05:07 -0500

    Created protocol for floor view controllers and connected all labels to their corresponding view controllers. Each view controller is able to do an HTTP Get request to the endpoint and receive data from the server

---
+++ BUSCA_Project/BUSCA App/Culc3rdFloor.swift
@@ -0,0 +1,83 @@
+//
+//  Culc3rdFloor.swift
+//  BUSCA App
+//
+//  Created by Aldo Mario Rogliero on 2/24/20.
+//  Copyright © 2020 Georgia Institute of Technology. All rights reserved.
+//
+
+
+import UIKit
+
+class Culc3rdFloor : UIViewController {
+    
+    @IBOutlet var tables : [UILabel]!
+    
+    var urlString : String = "https://fvqmb46jr5.execute-api.us-east-1.amazonaws.com/test/helloworld"
+    
+    override func viewDidLoad() {
+        updateLabels()
+        super.viewDidLoad()
+    }
+    
+    func updateLabels() {
+        for table in tables {
+            let status = getStatus(id: table.restorationIdentifier)
+            if (status == nil) {
+                table.text="⬛️"
+            } else if (status == true) {
+                table.text="🟩"
+            } else {
+                table.text="🟥"
+            }
+        }
+    
+    }
+    
+    func getStatus(id:String?) -> Bool? {
+        var isAvailable : Bool?
+        if (id != nil) {
+            // code for checking availability status
+            let session = URLSession.shared
+            
+            let url = URL(string: urlString)!
+            
+            
+            
+            let task = session.dataTask(with: url) { data, response, error in
+
+                if error != nil || data == nil {
+                    print("Client error!")
+                    return
+                }
+
+                guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                    print("Server error!")
+                    return
+                }
+
+                guard let mime = response.mimeType, mime == "application/json" else {
+                    print("Wrong MIME type!")
+                    return
+                }
+
+                do {
+                    
+                    var str = "Data is nil"
+                    if (data != nil) {
+                        str = String(decoding: data!, as: UTF8.self)
+                    }
+                    print(str)
+                    
+                    let json : NSDictionary = try JSONSerialization.jsonObject(with: data!, options: []) as! NSDictionary
+                    isAvailable = json.object(forKey: id!) as? Bool
+                } catch {
+                    print("JSON error: \(error.localizedDescription)")
+                }
+            }
+
+            task.resume()
+        }
+        return isAvailable
+    }
+}
---
+++ BUSCA_Project/BUSCA App/FloorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FloorViewController.swift
+//  BUSCA App
+//
+//  Created by Aldo Mario Rogliero on 2/25/20.
+//  Copyright © 2020 Georgia Institute of Technology. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol FloorViewController : UIViewController {
+    
+    var tables : [UILabel]! { get }
+    var urlString : String { get }
+    
+    func updateLabels() -> Void
+    func getStatus(id: String?) -> Bool?
+    
+}
